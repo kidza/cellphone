c8e962a0fca53f4fe2d2f1fd97ff7eaa013f0d60
dzaki <dzaivan@gmail.com>
2015-02-04 14:32:21 +0100

    Setting up Application and Cellphone modules

---
+++ module/Cellphone/config/module.config.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+return array(
+    'router' => array(
+        'routes' => array(
+            'home' => array(
+                'type' => 'Zend\Mvc\Router\Http\Literal',
+                'options' => array(
+                    'route'    => '/',
+                    'defaults' => array(
+                        'controller' => 'Cellphone\Controller\Index',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+            'cellphone' => array(
+                'type'    => 'Literal',
+                'options' => array(
+                    'route'    => '/cellphone',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'Cellphone\Controller',
+                        'controller'    => 'Index',
+                        'action'        => 'list',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'default' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '/[:controller[/:action]]',
+                            'constraints' => array(
+                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            ),
+                            'defaults' => array(
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'service_manager' => array(
+        'abstract_factories' => array(
+            'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
+            'Zend\Log\LoggerAbstractServiceFactory',
+        ),
+    ),
+    'controllers' => array(
+        'invokables' => array(
+            'Cellphone\Controller\Index' => 'Cellphone\Controller\IndexController'
+        ),
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+
+);

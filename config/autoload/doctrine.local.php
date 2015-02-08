adf8faf7fc741a79f33daca5b69933dfc3c327a6
dzaki <dzaivan@gmail.com>
2015-02-08 18:08:54 +0100

    Added local DB config file

---
+++ config/autoload/doctrine.local.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @ copyright Solera
+ * @ author Ivan Stankovic 
+ * @ created Feb 7, 2015
+ * @ description :
+ */ 
+return array (
+        'doctrine' => array (
+                'connection' => array (
+                        'orm_default' => array (
+                                'params' => array (
+                                        'user'     => 'DBusername',
+										'password' => 'DBpass',
+										'dbname'   => 'DBname',
+                                )
+                        )
+                )
+        )
+);
